b5a85ddbd367385bd231ec6de44ec7d22711b4f1
Sidney <user@SGD-MBP.local>
2016-06-07 15:18:41 -1000

    2nd Day 2nd Work

--- calculator.js
+++ calculator.js
@@ -6,6 +6,26 @@
  * @return {object} `calculator` object that can be used
  */
 
+/* function calculatorModule(){
+  var total = 0;
+  var memory = 0;
+  var calculator = {};
+  return calculator;
+ }
+*/
+
+var calculatorModule = (function() {
+  var total = 0;
+  var memory = 0;
+
+  return {
+    load: load,
+    getTotal: getTotal,
+    add: add,
+    substract: substract,
+    multiply: multiply,
+    divide:divide
+  };
 
   /**
    * sets the `total` to the number passed in
@@ -13,11 +33,21 @@
    * @return { Number }    current total
    */
 
+   function load(x){
+    total = x;
+    return total;
+   }
+
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
+
+   function getTotal(){
+      return total;
+   }
+
 
 
   /**
@@ -25,11 +55,21 @@
    * @param { Number } x
    */
 
+   function add(x){
+    x = x+total;
+    return x;
+   }
+
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
+
+   function substract(x){
+    x = x-total;
+    return x;
+   }
 
 
   /**
@@ -37,23 +77,36 @@
    * @param  { Number } x
    */
 
+   function multiply(x){
+    x = x*total;
+    return x;
+   }
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
+   function divide(x){
+    x = x/total;
+    return x;
+   }
 
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
+    function recallMemory(){
+      return memory;
+    }
 
 
   /**
    * Stores the value of `total` to `memory`
    */
-
+   function saveMemory(){
+      memory = total;
+   }
 
   /**
    * Clear the value stored at `memory`
@@ -63,3 +116,4 @@
    * Validation
    */
 
+});//THIS IS THE CLOSING BRACKETS FOR THE CALCULATORMODULE
